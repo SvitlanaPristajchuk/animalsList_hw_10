2a788de3125fed5baa73b761291cc610696c2691
Svitlana Pristajchuk <svetlana.pristajchuk@gmail.com>
2023-09-13 18:48:23 +0200

    add

--- index.js
+++ index.js
@@ -84,14 +84,33 @@ let vasjaHello = getName('Vasja')
 
 console.log(vasjaHello)*/
 
-
-let arr = [10, 20, 30];
+/* 
+let arr = [
+    [10, 20, 30],
+    [`dog`, `cat`, `lion`]
+]
+ */
 /* for (let i = 0; i < arr.length; i++) {
     document.write(`<p>${arr[i]}</p>`)
 } */
 
-arr.forEach(function(el) {
+/* arr.forEach(function(el) {
     // document.write(`<p>${arr[i]}</p>`)
     //console.log(el, index, list)
-    document.write(`<li>${el}</li>`);
-})
+    //document.write(`<li>${el}</li>`);
+    el.forEach(function(item) {
+        document.write(`<li>${item}</li>`);
+    })
+
+}) */
+
+
+let arr = [10, 20, -100, -1000, 35, 78]
+
+let filterArr = arr.filter(
+    function(el) {
+        return el > -1000
+
+    })
+
+console.log(filterArr)
